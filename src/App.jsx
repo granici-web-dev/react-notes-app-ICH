c7db9d60bc645db450fbfcab28b241912339d85c
Serghei_Granici <granici.design@gmail.com>
2026-03-11 13:48:57 +0100

    Fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css';
 import Header from './components/Header';
-import Footer from "./components/Footer";
+import Footer from './components/Footer';
 import Main from './components/Main';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col justify-between">
-      <div>
-        <Header />
-        <Main />
-      </div>
+      <Header />
+      <Main />
       <Footer />
     </div>
   );
